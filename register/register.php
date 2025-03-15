55afa4034010f08cba70588c3b020bf4d234e21b
agusbudionoprastyo <97543860+agusbudionoprastyo@users.noreply.github.com>
2025-03-15 17:08:13 +0700

    Update register.php

--- register/register.php
+++ register/register.php
@@ -58,7 +58,7 @@ try {
         $coupleUsername = generateCoupleUsername($coupleName);
         
         // Proses penyimpanan data untuk pasangan
-        $query = "INSERT INTO users (transaction_id, name, mantan, size, username, password) VALUES (?, ?, ?, ?, ?)";
+        $query = "INSERT INTO users (transaction_id, name, mantan, size, username, password) VALUES (?, ?, ?, ?, ?, ?)";
         $stmt = $conn->prepare($query);
         $stmt->bind_param("ssssss", $transactionId, $coupleName, $coupleMantan, $coupleSize, $coupleUsername, $password);
         $stmt->execute();
