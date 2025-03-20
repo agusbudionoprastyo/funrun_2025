3254313bbf77f65567415725e513ddae39e9862b
agusbudionoprastyo <97543860+agusbudionoprastyo@users.noreply.github.com>
2025-03-21 00:33:50 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -330,7 +330,7 @@ if (!isset($_SESSION['user_id'])) {
         const apiKey = "JkGJqE9infpzKbwD6QrmrciZPF1fwt";  // API Key kamu
         const sender = "628567868154"; // Nomor pengirim
         const recipientNumber = event.target.dataset.phone; // Nomor penerima yang diambil dari dataset tombol
-        const message = "*Pembayaran Anda telah diverifikasi!*\n\n Tunjukkan QR code ini kepada petugas kami saat pengambilan *BIB NUMBER* (nomor dada).\nTerima kasih atas partisipasi Anda,\nakses RunMap, https://funrun.dafam.cloud\n\n*Your payment has been verified!* Please show this QR code to our staff when collecting *BIB NUMBER* (race number).\nThank you for your participation.\naccess the RunMap, https://funrun.dafam.cloud ";
+        const message = "*Pembayaran Anda telah diverifikasi!*\nTunjukkan QR code ini kepada petugas kami saat pengambilan *NOMOR BIB* (nomor dada).\nTerima kasih atas partisipasi anda.\n\n*Your payment has been verified!*\nPlease show this QR code to our staff when collecting *BIB NUMBER* (race number).\nThank you for your participation.\n\naccess the RunMap\nhttps://funrun.dafam.cloud ";
 
         // Validasi jika nomor penerima ada
         if (!recipientNumber) {
